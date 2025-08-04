75d33ccdd8f4ac00a6284d6c5f0cc49dada186c8
aDisplayName <mattali@gmail.com>
2025-08-04 12:25:03 -0500

    fix: Static code check error

--- internal/registry/registry.go
+++ internal/registry/registry.go
@@ -272,23 +272,24 @@ func getImageSizeByManifestIndex(tt v1.ImageIndex) (int64, error) {
 	}
 
 	for _, child := range children {
-		switch typedChild := child.(type) {
+		child := child
+		switch child.(type) {
 		case v1.ImageIndex:
-			size, err := getImageSizeByManifestIndex(typedChild)
+			size, err := getImageSizeByManifestIndex(child.(v1.ImageIndex))
 			if err != nil {
 				return 0, err
 			}
 			totalSize += size
 
 		case v1.Image:
-			imageSize, err := getImageSizeByImageManifest(typedChild)
+			imageSize, err := getImageSizeByImageManifest(child.(v1.Image))
 			if err != nil {
 				return 0, err
 			}
 			totalSize += imageSize
 
 		case v1.Layer:
-			layerSize, err := typedChild.Size()
+			layerSize, err := child.(v1.Layer).Size()
 			if err != nil {
 				return 0, err
 			}
